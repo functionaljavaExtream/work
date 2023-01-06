95f72b0ff7813db1adce3dca18ce80772ff18f54
functionaljavaExtream <mapodaegyo12@gmail.com>
2023-01-06 15:09:33 +0900

    replacecode

--- src/sample01/MainClass.java
+++ src/sample01/MainClass.java
@@ -2,13 +2,7 @@ package sample01;
 
 public class MainClass {
 	public static void main(String[] args) {
-		System.out.println("헬로월드 ggd");
-		System.out.print("HELLO\n");
-		System.out.println("World");
-		
-		// ê¸°ë¥ : escape sequence
-		//      \n(개행), \b(backspace), \"(따옴표), \t(탭) 
-		System.out.println("\tHELLO\tWorld");
-		System.out.println("\tWorld\tHELLO");
+		System.out.println("헬로우 자바");
+		System.out.println("수정완료");
 	}
 }
